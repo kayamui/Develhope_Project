3795eeda041edc198dd9de58bd6e58b7e3bdf5ba
Muhammed Kaya <muhammedkaya65@gmail.com>
2022-11-24 21:32:44 +0300

    Just changed the COUNT to the SUM

--- week_2_homeworks.sql
+++ week_2_homeworks.sql
@@ -1,5 +1,5 @@
 ---- QUESTION 1: Which is / are the top suppliers (we have more products in stock) of Confections products?
-SELECT p.ProductID , s.CompanyName ,  p.ProductName , COUNT(od.Quantity) AS Quantity , c.CategoryName  FROM Categories c
+SELECT p.ProductID , s.CompanyName ,  p.ProductName , SUM(od.Quantity) AS Quantity , c.CategoryName  FROM Categories c
 JOIN Products p ON p.CategoryID  = c.CategoryID 
 JOIN Suppliers s ON s.SupplierID = p.SupplierID 
 JOIN "Order Details" od ON od.ProductID = p.ProductID 
@@ -10,7 +10,7 @@ ORDER BY s.CompanyName , Quantity  DESC
 --QUESTION 2: The company is studying to open a new city (to send goods) in Vilna and the market research teams requires to know which is the
 ---top sold product in a nearby city to start with more stock with this product. 
 ----Which is the most popular (purchased) product in Berlin?
-SELECT P.ProductID , p.ProductName, od.Quantity  ,o.ShipCity  FROM Orders o 
+SELECT P.ProductID , p.ProductName, SUM(od.Quantity)  ,o.ShipCity  FROM Orders o 
 JOIN "Order Details" od ON od.OrderID = o.OrderID
 JOIN Products p ON p.ProductID = od.ProductID 
 WHERE o.ShipCity = 'Berlin'
